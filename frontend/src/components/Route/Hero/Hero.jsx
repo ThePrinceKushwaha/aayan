1402ac750af861449aae96a8ac7afa151e669fc6
Prince Kushwaha <princekushwaha22@yahoo.com>
2024-04-23 22:11:12 +0545

    ffront-end updated

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -8,18 +8,18 @@ const Hero = () => {
       className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
       style={{
         backgroundImage:
-          `url(https://princekushwaha.com.np/hero-banner.png)`,
+          `url(https://princekushwaha.com.np/banner-1.png)`,
       }}
     >
       <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
         <h1
           className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
         >
-          Dashain Festive Sale <br /> Shopping with family.
+          {/* Dashain Festive Sale <br /> Shopping with family. */}
         </h1>
         <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
-        Festival a moment to spend most of the times with your loved ones {" "}
-          <br /> Do hassle free shopping with Home E-Shop.
+        {/* Festival a moment to spend most of the times with your loved ones {" "}
+          <br /> Do hassle free shopping with Home E-Shop. */}
         </p>
         <Link to="/products" className="inline-block">
             <div className={`${styles.button} mt-5`}>
--- frontend/src/static/data.js
+++ frontend/src/static/data.js
@@ -191,73 +191,47 @@ export const navItems = [
   export const categoriesData = [
     {
       id: 1,
-      title: "Computers and Laptops",
+      title: "Electrical",
       subTitle: "",
       image_Url:
-        "https://cdn.shopify.com/s/files/1/1706/9177/products/NEWAppleMacbookProwithM1ProChip14InchLaptop2021ModelMKGQ3LL_A_16GB_1TBSSD_custommacbd.jpg?v=1659592838",
+        "https://cdn.pixabay.com/photo/2019/09/27/08/06/electricity-4507838_1280.png",
     },
     {
       id: 2,
-      title: "cosmetics and body care",
+      title: "Sanitary",
       subTitle: "",
       image_Url:
-        "https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-07/kosme1.png",
+        "https://static.thenounproject.com/png/2604446-200.png",
     },
     {
       id: 3,
-      title: "Electric and Accessories",
+      title: "Machinery",
       subTitle: "",
       image_Url:
-        "https://img.freepik.com/free-vector/ordering-goods-online-internet-store-online-shopping-niche-e-commerce-website-mother-buying-babies-clothes-footwear-toys-infant-accessories_335657-2345.jpg?w=2000",
+        "https://banner2.cleanpng.com/20180712/ckf/kisspng-heavy-machinery-excavator-architectural-engineerin-fingernail-5b481ee5085c12.3006999215314531570343.jpg",
     },
     {
       id: 4,
-      title: "Cloths",
+      title: "Plumbing",
       subTitle: "",
       image_Url:
-        "https://www.shift4shop.com/2015/images/industries/clothing/clothing-apparel.png",
+        "https://w7.pngwing.com/pngs/713/566/png-transparent-computer-icons-plumber-plumbing-plumber-miscellaneous-blue-text.png",
     },
     {
       id: 5,
-      title: "Shoes",
+      title: "Construction Materials",
       subTitle: "",
       image_Url:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvBQPQMVNRd6TtDkGs2dCri0Y-rxKkFOiEWw&usqp=CAU",
+        "https://e7.pngegg.com/pngimages/680/1014/png-clipart-computer-icons-building-materials-architectural-engineering-cortaderia-selloana-building-engineering.png",
     },
     {
       id: 6,
-      title: "Gifts",
+      title: "Tools and Accessories",
       subTitle: "",
       image_Url:
-        "https://securecdn.pymnts.com/wp-content/uploads/2014/11/Gifts-Photo-700x489.jpg",
+        "https://uxwing.com/wp-content/themes/uxwing/download/tools-equipment-construction/tools-icon.png",
     },
-    {
-      id: 7,
-      title: "Pet Care",
-      subTitle: "",
-      image_Url: "https://cdn.openpr.com/T/c/Tc15444071_g.jpg",
-    },
-    {
-      id: 8,
-      title: "Mobile and Tablets",
-      subTitle: "",
-      image_Url:
-        "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mpwp3tua-apple-iphone-14-256gb-mavi-mpwp3tua-637986832343472449.jpg",
-    },
-    {
-      id: 9,
-      title: "Music and Gaming",
-      subTitle: "",
-      image_Url:
-        "https://static.vecteezy.com/system/resources/previews/011/996/555/original/3d-black-headphone-illustration-ecommerce-icon-png.png",
-    },
-    {
-      id: 10,
-      title: "Others",
-      subTitle: "",
-      image_Url:
-        "https://searchspring.com/wp-content/uploads/2022/10/Hero-Image-Platform-Others-2.png",
-    },
+    
   ];
   
   // product Data
